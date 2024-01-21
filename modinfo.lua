c5a9b51b51cad4aab135a8ce5ef875e338f500cf
genchsusu <pctxiaozhu@gmail.com>
2024-01-21 20:20:35 +0800

    feat: add dst_no_spacing, add  dst_marine_engineering/preview.png

---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+name = "存图片"
+author = 'OpenSource'
+version = '1.0'
+description = "存图片"
+
+forumthread = ""
+	
+api_version = 10
+
+dst_compatible = false
+client_only_mod = false
+all_clients_require_mod = false
+
+priority = 0.1
+
+configuration_options = {}
